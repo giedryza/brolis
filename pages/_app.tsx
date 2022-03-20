b00065831dfa2696628f50e7b5127371b0752397
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-03-20 14:17:32 +0200

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import '../styles/index.scss';
 
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
